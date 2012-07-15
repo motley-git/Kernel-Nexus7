31d3897739f39b02af134bc15caa7062cb779a82
Colin Cross < Colin.Cross@google.com>
2012-07-15 14:10:41 -0400

    ARM: vfp: only clear vfp state for current cpu in vfp_pm_suspend vfp_pm_suspend runs on each cpu, only clear the hardware state pointer for the current cpu.  Prevents a possible crash if one cpu clears the hw state pointer when another cpu has already checked if it is valid.

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -461,7 +461,7 @@ static int vfp_pm_suspend(void)
 	}
 
 	/* clear any information we had about last context state */
-	memset(vfp_current_hw_state, 0, sizeof(vfp_current_hw_state));
+	last_VFP_context[ti->cpu] = NULL;
 
 	return 0;
 }
