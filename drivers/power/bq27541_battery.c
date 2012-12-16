69b6ffa72f547e41f462e5a46b419d77069cc650
Entropy512 <atd7@cornell.edu>
2012-12-16 18:52:01 -0500

    bq27541: Add current_now property The current is returned by the charger as a twos complement signed integer in milliamps.  Applications such as CurrentWidget expect this to be microamps.
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>
    Change-Id: I053b3d1f24ec04391e982fdcb4b3486241c3aa34

--- drivers/power/bq27541_battery.c
+++ drivers/power/bq27541_battery.c
@@ -142,6 +142,7 @@ static enum power_supply_property bq27541_properties[] = {
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_TECHNOLOGY,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
+	POWER_SUPPLY_PROP_CURRENT_NOW,
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_TEMP,
 };
@@ -502,6 +503,20 @@ static int bq27541_get_psp(int reg_offset, enum power_supply_property psp,
 		}
 		BAT_NOTICE("voltage_now= %u uV\n", val->intval);
 	}
+
+	if (psp == POWER_SUPPLY_PROP_CURRENT_NOW) {
+		val->intval = rt_value;
+		/* Returns a signed 16-bit value in mA */
+		if (val->intval & 0x8000) {
+			/* Negative */
+			val->intval = ~val->intval & 0x7fff;
+			val->intval++;
+			val->intval *= -1;
+		}
+		val->intval *= 1000;
+		BAT_NOTICE("current_now= %d uA\n", val->intval);
+	}
+
 	if (psp == POWER_SUPPLY_PROP_STATUS) {
 		ret = bq27541_device->bat_status = rt_value;
 		static char *status_text[] = {"Unknown", "Charging", "Discharging", "Not charging", "Full"};
@@ -560,7 +575,7 @@ static int bq27541_get_psp(int reg_offset, enum power_supply_property psp,
 			}
 		}
 		bq27541_device->old_temperature = val->intval = ret;
-		BAT_NOTICE("temperature= %u (0.1¢XC)\n", val->intval);
+		BAT_NOTICE("temperature= %u (0.1ï¿½XC)\n", val->intval);
 	}
 	return 0;
 }
