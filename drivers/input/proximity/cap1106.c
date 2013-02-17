890bb106a305131e6b8fae27fdc2445d207b0421
tryout_chen <tryout_chen@asus.com>
2013-02-17 16:12:02 -0500

    Proximity: Enable sensor by it's previous RIL setting when device resume from suspend.
    Change-Id: Id5b0bfaa79d62e5c41fa704470cf82bf7a0fefac

--- drivers/input/proximity/cap1106.c
+++ drivers/input/proximity/cap1106.c
@@ -72,6 +72,7 @@ static int prev_c6_status = 0;
 static int c2_acc_cnt = 0;
 static int c6_acc_cnt = 0;
 static int acc_limit = 10;
+static int force_enable = 1;
 
 /*----------------------------------------------------------------------------
  ** FUNCTION DECLARATION
@@ -472,6 +473,7 @@ static ssize_t store_sensor_onoff(struct device *dev, struct device_attribute *a
         return -EINVAL;
 
     mutex_lock(&prox_mtx);
+    force_enable = enable;
     cap1106_enable_sensor(client, enable);
     mutex_unlock(&prox_mtx);
 
@@ -882,7 +884,8 @@ static int cap1106_suspend(struct i2c_client *client, pm_message_t mesg)
 {
     PROX_DEBUG("+\n");
     mutex_lock(&prox_mtx);
-    cap1106_enable_sensor(client, 0);
+    if (force_enable)
+        cap1106_enable_sensor(client, 1);
     mutex_unlock(&prox_mtx);
     PROX_DEBUG("-\n");
     return 0;
